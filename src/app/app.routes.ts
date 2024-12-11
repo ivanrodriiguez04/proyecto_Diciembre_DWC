d217dd77335c0cf218fe632940f4a98839a1aafe
ivanrodriiguez04 <103932821+ivanrodriiguez04@users..noreplygithub.com>
2024-12-11 13:14:07 +0100

    v1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { SeriesComponent } from './series/series.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'peliculas',component:PeliculasComponent},
+    {path:'series',component:SeriesComponent},
+
+];
